096d98bc61c6b4e4fb0c981e7e99a03211ca2564
Travis Osteen <t.osteen89@gmail.com>
2020-06-06 12:28:12 -0500

    Testing Github Pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "docs"),
+        path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
         chunkFilename: "[id].js",
         publicPath: "",
