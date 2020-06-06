6cf81b931a583d6297964199f18b6ebd9e7b394f
Travis Osteen <t.osteen89@gmail.com>
2020-06-06 12:19:24 -0500

    Changing build file to docs

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "docs"),
         filename: "bundle.js",
         chunkFilename: "[id].js",
         publicPath: "",
